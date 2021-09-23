1bb2eee7e5a78950fd38f973afb923c469fd81b9
Porter Morrison <port.morrison@gmail.com>
2021-09-23 12:59:05 -0600

    final

---
+++ profile.js
@@ -0,0 +1,24 @@
+
+
+
+document.getElementById("color").addEventListener("click", function() {
+  alert("blue");
+});
+
+
+
+document.getElementById("place").addEventListener("click", function() {
+  alert("Manti La Sal Mountain Range");
+});
+
+
+
+document.getElementById("ritual").addEventListener("click", function() {
+  alert("Stretching in the mourning");
+});
+
+
+
+
+
+
